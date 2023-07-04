dff5781f0e740ed3beb7a34f9c8239c8c5001f5b
Carl Fairclough <hi@carlfairclough.me>
2023-07-04 05:07:00 +0100

    Typing comments

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,41 +1,41 @@
-
 import { fromHex } from "@3id/common";
-import {ThreeIdProvider} from "@3id/did-provider";
-import { CeramicClient } from '@ceramicnetwork/http-client'
+import { ThreeIdProvider } from "@3id/did-provider";
+import { CeramicClient } from "@ceramicnetwork/http-client";
 import { DID } from "dids";
 
 import { getResolver as get3IDResolver } from "@ceramicnetwork/3id-did-resolver";
 
 export const connect3ID = async (seed: string) => {
-
-  
   // First clear all local permissions so accounts can switch
   Object.keys(localStorage)
     .filter((x) => x.startsWith("3id_permission_"))
     .forEach((x) => localStorage.removeItem(x));
 
   const _seed = fromHex(seed);
-  const ceramic = new CeramicClient(process.env.NEXT_PUBLIC_CERAMIC_ENDPOINT)
+  const ceramic = new CeramicClient(process.env.NEXT_PUBLIC_CERAMIC_ENDPOINT);
 
   const threeID = await ThreeIdProvider.create({
+    //@ts-ignore // expects CeramicApi. Why?
     ceramic: ceramic,
     seed: _seed,
     getPermission: (request: any) => Promise.resolve(request.payload.paths),
   });
 
+  //@ts-ignore // expects CeramicApi. Why?
   const resolver = get3IDResolver(ceramic);
 
   const did = new DID({
-    //@ts-ignore
+    //@ts-ignore // DidProvider is not assignable to type DIDProvider. Why?
     provider: threeID.getDidProvider(),
+    //@ts-ignore // Mismatched types. Expects ResolverRegistry. Why?
     resolver: resolver,
   });
 
- let authenticated = false
+  let authenticated = false;
 
   try {
     await did.authenticate();
-    authenticated = true
+    authenticated = true;
   } catch (err) {
     console.error(`Failed to authenticate ${did.id}`, err);
   }
